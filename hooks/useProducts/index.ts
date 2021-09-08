b68ba3789a8473726a454624ca41c3eada8812b8
Aaron Smulktis <aaronsmulktis@gmail.com>
2021-09-07 23:34:04 -0700

    setup some CORs proxies for direct Spree API (non-SDK) requests, add SWF pkg

--- hooks/useProducts/index.ts
+++ hooks/useProducts/index.ts
@@ -11,7 +11,7 @@ const fetchProducts = async (page: number = 1) => {
   const storage = (await import("../../config/storage")).default;
   const token = await storage.getToken();
   const response = await spreeClient.products.list({
-    bearerToken: token ? token.access_token : undefined,
+    bearerToken: token ? token.access_token : undefined
     // include: "images"
   });
   if (response.isSuccess()) {
